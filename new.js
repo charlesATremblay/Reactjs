e1bac372374b0e0a7b93c4240ba26d99d9719847
charlesATremblay <charlesantoine.t@gmail.com>
2021-01-16 15:13:47 -0500

    enlever la partie greeter

--- new.js
+++ new.js
@@ -4,7 +4,3 @@ negativeNumbers[0] = 4
 negativeNumbers[1] = 3
 negativeNumbers[2] = 2
 console.log(negativeNumbers);
-
-const greeter="Hello";
-greeter.replace("He", "a");
-console.log(greeter.replace("He", "a"));

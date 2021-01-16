da3f76d9220c801459a25fb307b4cbfb4b11dc35
charlesATremblay <charlesantoine.t@gmail.com>
2021-01-16 15:00:13 -0500

    new file:   new.js

---
+++ new.js
@@ -0,0 +1,10 @@
+const negativeNumbers = [];
+//negativeNumbers=[1,3,4];
+negativeNumbers[0] = 4
+negativeNumbers[1] = 3
+negativeNumbers[2] = 2
+console.log(negativeNumbers);
+
+const greeter="Hello";
+greeter.replace("He", "a");
+console.log(greeter.replace("He", "a"));

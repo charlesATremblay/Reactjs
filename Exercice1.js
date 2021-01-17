11c6b440614fdbc7abaa3033ba3e5dff72866478
charlesATremblay <charlesantoine.t@gmail.com>
2021-01-17 13:07:24 -0500

    React.js in 30 days, Day 1, Exercice 3, Level 1

---
+++ Exercice1.js
@@ -0,0 +1,42 @@
+/*
+1. Create an empty object called dog
+2. Print the the dog object on the console
+3. Add name, legs, color, age and bark properties for the dog object. 
+The bark property is a method which return woof woof
+4. Get name, legs, color, age and bark value from the dog object
+5. Set new properties the dog object: breed, getDogInfo
+*/
+
+
+const dog={
+    name:'Gimli',
+    legs: 4,
+    color:'white and brown',
+    age: 6,
+    bark: function(){
+        return `woof woof`;
+    },
+    getName: function(){
+        return `${this.name}`
+    },
+    getLegs: function(){
+        return `${this.legs}`
+    },
+    getColor:function(){
+        return `${this.color}`
+    },
+    getAge: function(){
+        return `${this.age}`
+    },
+}
+dog.breed='Cavachon'
+dog.getDogInfo = function(){
+    let name= `${this.getName()}`
+    let legs= `${this.getLegs()}`
+    let color=`${this.getColor()}`
+    let age= `${this.getAge()}`
+    let statement = `${name} is a ${this.breed}.\nHe has ${legs} legs and he is ${color}.\nHe is ${age} years old.`
+    return statement
+}
+console.log(dog.getDogInfo())
+
